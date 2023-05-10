c512ae472c6a41e25b9eb0d713fb9a6671ae9e61
Rob Ranf <rob@emiyaconsulting.com>
2023-05-09 18:59:42 -0700

    Adding broken egg class

---
+++ HeadFirstBirdEggsCL/BrokenEgg.cs
@@ -0,0 +1,12 @@
+// HeadFirstBirdEggsCL
+// BrokenEgg.cs
+// Created: 05 09 2023
+// Author: Rob Ranf (robranf)
+// (C) 2023 Emiya Consulting, LLC
+
+namespace HeadFirstBirdEggsCL;
+
+public class BrokenEgg
+{
+    
+}
